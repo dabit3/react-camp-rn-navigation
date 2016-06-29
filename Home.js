9879518662ac108509089b3eb32ebc626507fedb
dabit3 <dabit3@gmail.com>
2016-06-28 20:41:24 -0500

    added title to about page

--- Home.js
+++ Home.js
@@ -6,6 +6,7 @@ class Home extends Component {
   navigate () {
     this.props.navigator.push({
       component: About,
+      title: 'About',
       passProps: {
         name: 'React Camp'
       }
