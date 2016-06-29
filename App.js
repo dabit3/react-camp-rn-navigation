999a6af888738e42680a1dd3c50a215e49377739
dabit3 <dabit3@gmail.com>
2016-06-28 21:53:59 -0500

    updating

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { AppRegistry } from 'react-native'
+
+import configureStore from './app/store/configureStore'
+const store = configureStore()
+
+import NavigationRootContainer from './app/containers/navRootContainer'
+import { Provider } from 'react-redux'
+
+const App = () => (
+  <Provider store={store}>
+    <NavigationRootContainer />
+  </Provider>
+)
+AppRegistry.registerComponent('RNComprehensiveNavigation', () => App)
